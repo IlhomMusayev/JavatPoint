c49e209cfa64bc8671960af00d82c32de0f9ed91
IlhomMusayev <ilhommusayev2001@gmail.com>
2022-01-13 22:31:27 +0500

    render subject, and tutorials tutorial page

--- controllers/TutorialControllers.js
+++ controllers/TutorialControllers.js
@@ -1,22 +1,93 @@
 module.exports = class TutorialController {
     static async TutorialBySubjectController(req, res, next) {
-       try {
+        try {
             const language_slug = req.params.language_slug
-            
-            const language_id = await req.db.language.findOne({ 
+            const tutorial_default = await req.db.tutorial.findOne({
                 raw: true,
-                where:{
+                order: [['updatedAt', 'DESC']]
+            })
+
+            const tutorial_slug = req.params.tutorial_slug || tutorial_default.tutorial_slug
+
+            const language_id = await req.db.language.findOne({
+                raw: true,
+                where: {
                     language_slug: language_slug
                 },
             })
 
-
+            const tutorials = await req.db.tutorial.findAll({
+                where: {
+                    tutorial_slug: tutorial_slug,
+                }
+            })
             const subjects = await req.db.subject.findAll({
                 where: {
                     language_id: language_id.language_id
                 },
-                include: [
+                include: [{
+                        model: req.db.language
+                    },
                     {
+                        model: req.db.tutorial
+                    }
+                ],
+                order: [['updatedAt', 'DESC']]
+            })
+
+            // <% tutorial.language.dataValues.forEach(language => {%> <%=language.language_slug%>  <% }) %>
+
+            const data1 = []
+
+            subjects.forEach(subject => {
+                data1.push(subject.dataValues)
+                // console.log(subject.dataValues.language.dataValues.language_slug);
+            })
+
+
+            data1.forEach(element => {
+                console.log(element.language.dataValues.language_slug);
+            });
+            const data2 = []
+
+            tutorials.forEach(tutorial => {
+                data2.push(tutorial.dataValues)
+            })
+
+          
+
+            res.render('tutorial', {
+                tutirials: data2,
+                subjects: data1
+            })
+        } catch (error) {
+            console.log(error);
+            next(error)
+        }
+    }
+
+    static async TutorialGetController(req, res, next) {
+        try {
+            const language_slug = req.params.language_slug
+            const tutorial_slug = req.params.tutorial_slug
+
+            const language_id = await req.db.language.findOne({
+                raw: true,
+                where: {
+                    language_slug: language_slug
+                },
+            })
+
+            const tutorials = await req.db.tutorial.findAll({
+                where: {
+                    tutorial_slug: tutorial_slug,
+                }
+            })
+            const subjects = await req.db.subject.findAll({
+                where: {
+                    language_id: language_id.language_id
+                },
+                include: [{
                         model: req.db.language
                     },
                     {
@@ -27,59 +98,27 @@ module.exports = class TutorialController {
 
             // <% tutorial.language.dataValues.forEach(language => {%> <%=language.language_slug%>  <% }) %>
 
-            const data = []
+            const data1 = []
 
             subjects.forEach(subject => {
-                data.push(subject.dataValues)
+                data1.push(subject.dataValues)
             })
+            // //  <% tutorial.language.dataValues.forEach(language => {%> <%=language.language_slug%>  <% }) %>
+            const data2 = []
+
+            tutorials.forEach(tutorial => {
+                data2.push(tutorial.dataValues)
+            })
+
+            console.log(data1);
 
             res.render('tutorial', {
-                subjects: data
+                tutirials: data2,
+                subjects: data1
             })
-       } catch (error) {
-           next(error)
-       }
-    }
-
-    static async TutorialGetController(req, res, next) {
-        try {
-             const language_slug = req.params.language_slug
-             const subject_slug = req.params.subject_slug
-                         
-             const language_id = await req.db.language.findOne({ 
-                 raw: true,
-                 where:{
-                     language_slug: language_slug
-                 },
-             })
-
-             const subject_id = await req.db.subject.findAll({ 
-                raw: true,
-                where:{
-                    subject_slug: subject_slug
-                },
-            })
-
-             const tutorial = await req.db.tutorial.findAll({
-                 where: {
-                     language_id: language_id.language_id,
-                     subject_id: subject_id.subject_id
-                 }
-             })
-             console.log(tutorial);
-            // //  <% tutorial.language.dataValues.forEach(language => {%> <%=language.language_slug%>  <% }) %>
- 
-            //  const data = []
- 
-            //  subjects.forEach(subject => {
-            //      data.push(subject.dataValues)
-            //  })
- 
-             res.render('tutorial', {
-                //  subject: data
-             })
         } catch (error) {
+            console.log(error);
             next(error)
         }
-     }
+    }
 }
--- routes/TutorilRoute.js
+++ routes/TutorilRoute.js
@@ -2,6 +2,6 @@ const router = require("express").Router()
 const { TutorialGetController, TutorialBySubjectController } = require("../controllers/TutorialControllers")
 
 router.get('/:language_slug',  TutorialBySubjectController) 
-router.get('/:language_slug/:subject_slug',  TutorialGetController) 
+router.get('/:language_slug/:tutorial_slug',  TutorialGetController) 
 
 module.exports = router
